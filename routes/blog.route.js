df1595160802feb490117aac3177c17c67e7117d
Akash Gautam <akashgautam01584@gmail.com>
2024-03-12 12:38:18 +0530

    comments/view comments/view blog

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -3,6 +3,7 @@ const path = require("path");
 const router = Router();
 const multer = require("multer");
 const Blog = require("../models/blog.model");
+const Comment = require("../models/comments.model");
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
@@ -20,6 +21,26 @@ router.get("/add-new", (req, res) => {
 	return res.render("addBlog", {
 		user: req.user,
 	});
+});
+
+router.get("/:id", async (req, res) => {
+	const blog = await Blog.findById(req.params.id).populate("createdBy");
+	const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy");
+	return res.render("Blog", {
+		user: req.user,
+		blog,
+		comments,
+	});
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+	const comment = await Comment.create({
+		content: req.body.content,
+		blogId: req.params.blogId,
+		createdBy: req.user._id,
+	});
+
+	return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
